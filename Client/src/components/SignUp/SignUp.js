d3db07056c7bfdc175ff845a7b4104c29c173c3a
RufatEskendarov <rufat.eskendarov@gmail.com>
2023-04-16 21:55:49 -0300

    Updated SignUp

--- Client/src/components/SignUp/SignUp.js
+++ Client/src/components/SignUp/SignUp.js
@@ -1,77 +1,120 @@
 import React, { Fragment, useState } from "react";
 import "./SignUp.css";
+import { emailValidator } from "../../helpers/regexValidator";
 
 const SignUp = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [nameIsValid, setNameIsValid] = useState(true);
   const [enteredEmail, setEnteredEmail] = useState("");
-  const [enteredUserName, setUserName] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
+  const [emailIsValid, setEmailIsValid] = useState(true);
   const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
-  const [formIsValid, setFormIsValid] = useState(false);
- 
-  const [showBtn, setShowBtn] = useState("");
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+  const [passwordIsValid, setPasswordIsValid] = useState(true);
 
-    setFormIsValid(
-      event.target.value.includes("@") && enteredPassword.trim().length > 6
-    );
+  //E-mail validation
+  const emailHandler = (event) => {
+    setEnteredEmail(event.target.value.trim());
+    !emailValidator.test(enteredEmail)
+      ? setEmailIsValid(false)
+      : setEmailIsValid(true);
   };
 
-  const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+  //Entered User name validation
+  function nameHandler(e) {
+    const item = e.target.value.trim();
+    setEnteredName(item);
+    if (item.length < 1) {
+      setNameIsValid(false);
+    } else {
+      setNameIsValid(true);
+    }
+  }
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes("@")
-    );
-  };
+  //Password sign up validation
+  function passwordHandler(e) {
+    const item = e.target.value.trim();
+    setEnteredPassword(item);
+    if (item.length < 8) {
+      setPasswordIsValid(false);
+    } else {
+      setPasswordIsValid(true);
+    }
+  }
 
-  const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
-  };
-  const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
-  };
+  //Change page to SingIN
+  function signInHandler(e) {
+    e.preventDefault();
+    document.location.replace("/login");
+  }
 
-  const submitHandler = (event) => {
+  const sigUpHandler = async function (event) {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    if (
+      emailIsValid &&
+      passwordIsValid &&
+      enteredEmail !== "" &&
+      enteredPassword !== ""
+    ) {
+      const response = await fetch("/api/users/signup", {
+        method: "POST",
+        body: JSON.stringify({
+          name: enteredName,
+          email: enteredEmail,
+          password: enteredPassword,
+          loggedIn: true,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+        document.location.replace("/home");
+        console.log(response);
+      } else {
+        alert("Failed to login");
+      }
+    } else {
+      alert("Please check input fields again");
+    }
   };
 
   return (
     <Fragment>
-      <form className="sign-up__form" onSubmit={submitHandler}>
+      <form className="sign-up__form" onSubmit={sigUpHandler}>
         <label>User Name</label>
         <input
-         className="userEmailInput"
-         type="email"
-         value={enteredUserName}
-         onChange={emailChangeHandler}
-         onBlur={validateEmailHandler}
-         placeholder="Enter User Name"
+          className="userEmailInput"
+          type="text"
+          onChange={nameHandler}
+          placeholder="Enter User Name"
         ></input>
         <label>Email</label>
         <input
           className="userEmailInput"
           type="email"
-          value={enteredEmail}
-          onChange={emailChangeHandler}
-          onBlur={validateEmailHandler}
-          placeholder="Enter email"
+          onChange={emailHandler}
+          placeholder="example@example.com"
         ></input>
         <label>Password</label>
         <input
           className="userPwInput"
           type="password"
-          value={enteredPassword}
-          onChange={passwordChangeHandler}
-          onBlur={validatePasswordHandler}
-          placeholder="Enter Password"
+          onChange={passwordHandler}
+          placeholder="Enter your Password"
         ></input>{" "}
         <p></p>
-        <p></p>
-        <button  type="button">
-          SignUp{" "}
+        {!passwordIsValid ? (
+          <p>Password must be at least 8 characters long </p>
+        ) : (
+          ""
+        )}
+        {!emailIsValid ? (
+          <p>Wrong format for e-mail address (example: example@email.com)</p>
+        ) : (
+          ""
+        )}
+        {!nameIsValid ? <p>Name field can not be empty</p> : ""}
+        <button type="submit">SignUp </button>
+        <button onClick={signInHandler} type="button">
+          Or Login
         </button>
       </form>
     </Fragment>
