3772ace82d139beaa77739bc84a7bb441130078d
RufatEskendarov <rufat.eskendarov@gmail.com>
2023-04-17 23:10:06 -0300

    Updated Homepage

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,19 +1,31 @@
 import Header from "../components/Headers/Header";
 import Ingredients from "../components/Ingredients/Ingredients";
+import WeclomeMessage from "../components/Ingredients/WelcomeMessage";
+import ErrorMessage from "../components/Ingredients/ErrorMessage";
 import Preview from "../components/Recipe/Preview";
 import "./../styles/main.scss";
 import { useState } from "react";
 
 function Home() {
-
-const [search, setSearchRes] = useState();
-const [att, setAtt] = useState();
+  const [search, setSearchRes] = useState();
+  const [att, setAtt] = useState();
 
   return (
     <div className="container">
-      <Header setSearchRes = {setSearchRes}/>
-      <Preview searchRes = {search} setAtt = {setAtt}/>
-      <Ingredients attRes = {att}/>
+      <Header setSearchRes={setSearchRes} />
+      <Preview searchRes={search} setAtt={setAtt} />
+
+      {search === undefined ? (
+        <WeclomeMessage />
+      ) : (
+        <>
+          {search.length === 0 ? (
+            <ErrorMessage />
+          ) : (
+            <Ingredients attRes={att} />
+          )}
+        </>
+      )}
     </div>
   );
 }
