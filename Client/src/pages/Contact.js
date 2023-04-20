9152558f5d83d2c7e1a647844b463eebd44ff5e3
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-04-20 03:22:10 +0300

    fixed validation

--- Client/src/pages/Contact.js
+++ Client/src/pages/Contact.js
@@ -1,65 +1,63 @@
 import React, { useState } from 'react';
-import "./../styles/main.scss"
+import '../styles/main.scss'
 
- function Contact() {
-    const validateEmail = (email) => {
-        const emailRegex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-        return emailRegex.test(String(email).toLowerCase())
+function Contact() {
+  const validateEmail = (email) => {
+    const emailRegex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+    return emailRegex.test(String(email).toLowerCase())
+  }
+
+  const [formState, setformState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+
+  const [errMsg, seterrMsg] = useState("")
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!errMsg) {
+      console.log("submitForm", formState)
+    }
+  }
+
+
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      const validEmail = validateEmail(e.target.value)
+      if (!validEmail) {
+        seterrMsg("enter valid email")
+
+      }
+      else {
+        seterrMsg("")
+      }
+    }
+    else {
+      if (!e.target.value.length) {
+        seterrMsg(`${e.target.name} is required`)
+
+      }
+      else {
+        seterrMsg("")
+      }
     }
 
-    const [formState, setformState] = useState({
-        name: "",
-        email: "",
-        message: "",
-    })
-    const [errMsg, seterrMsg] = useState("")
-
-    const { name, email, message } = formState;
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if (!errMsg) {
-            console.log("submitForm", formState)
 
 
-        }
+    if (!errMsg) {
+      setformState({ ...formState, [e.target.name]: e.target.value })
     }
+  };
 
-    const handleChange = (e) => {
-        if (e.target.name === "email") {
-            const validEmail = validateEmail(e.target.value)
-            if (!validEmail) {
-                seterrMsg("Enter valid email")
 
-            }
-            else {
-                seterrMsg("")
-            }
-        }
-        else {
-            if (!e.target.value.length) {
-                seterrMsg(`${e.target.name} is required`)
-
-            }
-            else {
-                seterrMsg("")
-            }
-        }
-
-        if (!errMsg) {
-            setformState({ ...formState, [e.target.name]: e.target.value })
-        }
-    }
-    return (
-
-        < div className = "contactForm" >
-          
-     
-
-            <div className="background">
-        <div className="containerContact" onSubmit={handleSubmit}>
-        
-       
-      
+  return (
+    < div className="contactForm" >
+      <div className="background">
+        <div className="containerContact" >
           <div className="screen">
             <div className="screen-header">
               <div className="screen-header-left">
@@ -79,50 +77,45 @@ import "./../styles/main.scss"
                   <span>CONTACT</span>
                   <span>US</span>
                 </div>
-                <div className="app-contact">CONTACT INFO : +506 000 000</div>
+
               </div>
               <div className="screen-body-item">
                 <div className="app-form">
                   <div className="app-form-group">
-                    <input type ="text" id ="name" className="app-form-control" placeholder=" NAME" 
-                   onBlur={handleChange} defaultValue={name}></input>
+                    <input type="text" id="name" name="name" className="app-form-control" placeholder=" NAME"
+                      onBlur={handleChange} defaultValue={name}></input>
+
                   </div>
                   <div className="app-form-group">
-                    <input type ="text" className="app-form-control" placeholder="EMAIL"
-                    onBlur={handleChange} defaultValue={email}></input>
+                    <input type="text" name="email" className="app-form-control" placeholder=" EMAIL"
+                      onBlur={handleChange} defaultValue={email}></input>
                   </div>
                   <div className="app-form-group">
-                    <input className="app-form-control" placeholder="CONTACT NO"/>
+                    <input className="app-form-control" placeholder="CONTACT NO" />
                   </div>
                   <div className="app-form-group message">
-                    <input className="app-form-control" placeholder="MESSAGE"
-                    onBlur={handleChange} defaultValue={message}/>
+                    <input className="app-form-control" name="message" placeholder="MESSAGE"
+                      onBlur={handleChange} defaultValue={message} />
                   </div>
                   <div className="app-form-group buttons">
-                    <button className="app-form-button">CANCEL</button>
-                    <button className="app-form-button" type = "submit">SEND</button>
+                    {/* <button className="app-form-button" onClick={handleCancel} >CANCEL</button> */}
+                    <button className="app-form-button" type="submit" onSubmit={handleSubmit}>SEND</button>
                   </div>
-                  
-               
-                  
+
+                  {errMsg && (
+                    <div style={{ color: "red", fontSize: 20 }}
+                      class="status">{errMsg} </div>
+                  )}
+
+                </div>
               </div>
             </div>
           </div>
-         
         </div>
       </div>
-      </div>
-      
-    
-                {errMsg && (
-            <div style = {{color:"red"}}
-             class="status">{errMsg} </div>
-            )}
-      </div>
-    
-     
-    )
-
+    </div>
+  )
 }
+
 
 export default Contact;
